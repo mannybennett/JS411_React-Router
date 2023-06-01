5bacf628dbfca13853cc5706a555a4416d74b34d
mannybennett <mannybennett99@gmail.com>
2023-05-31 22:10:32 -0400

    Complete MUI styling

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import cars from '../cars.json'
+import React from 'react';
+import cars from '../cars.json';
 // Import {useParams} from "react-router-dom" here //
+import { useParams } from "react-router-dom";
 
 // import MUI components here //
 // Container, Paper, Chip //
+import { Container, Paper, Chip } from '@mui/material';
 
-const Car = (props) => {
+const Car = () => {
+    const { id } = useParams()
+    const car = cars[id-1]
+    const keys = Object.keys(car)
+
     return (
-        <h1>A specific car</h1>
+        <Container maxWidth='xs' sx={{ marginTop: 20 }}>
+            <Paper sx={{ padding: 8, width: '100%', height: '100%' }}>
+                {console.log(keys)}
+                <h2>{car[keys[1]]}</h2>
+                {keys.map((info, idx) => {
+                    return (
+                    <Chip sx={{ backgroundColor: '#E1E0E1' }} key={idx} label={`${info}:${car[info]}`} />
+                    )
+                })}
+            </Paper>
+        </Container>
     )
-}
+};
 
-export default Car
+export default Car;
