f59674d7bb83093f699ae28aa4984fdc1c9bdb64
mannybennett <mannybennett99@gmail.com>
2023-05-31 22:13:57 -0400

    Updates

---
+++ precache-manifest.9ae23fd72d9a43632bdd7c817a7f30a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f9c6b9d5d879c4b0023d808fd3e2a1",
+    "url": "/JS411_React-Router/index.html"
+  },
+  {
+    "revision": "c3b71231489321ac8b59",
+    "url": "/JS411_React-Router/static/css/main.8e4de9ae.chunk.css"
+  },
+  {
+    "revision": "f25404a53eeb01f8b84b",
+    "url": "/JS411_React-Router/static/js/2.af911115.chunk.js"
+  },
+  {
+    "revision": "c3b71231489321ac8b59",
+    "url": "/JS411_React-Router/static/js/main.93b51529.chunk.js"
+  },
+  {
+    "revision": "e14a9c8fce7c9225fb32",
+    "url": "/JS411_React-Router/static/js/runtime~main.708af664.js"
+  }
+]);
